50f7c8870ccd7ef8771ac98be8f27c62fea59e60
Ev Bogue <ev@evbogue.com>
2018-04-20 21:10:14 -0500

    float timestamp right

--- render.js
+++ render.js
@@ -7,9 +7,9 @@ var avatar = require('./avatar')
 module.exports = function (msg) {
   if (msg.value.content.type == 'post') {
     return h('div.message__content',
-      h('span.timestamp', h('a', {href: msg.key}, human(new Date(msg.value.timestamp)))),
-      avatar.name(msg.value.author),
-      msg.value.content.text
+      h('span.avatar', h('a', {href: '#' + msg.value.author}, avatar.name(msg.value.author))),
+      h('span.timestamp', h('a', {href: '#' + msg.key}, human(new Date(msg.value.timestamp)))),
+      h('div.message__body', msg.value.content.text)
     )
   } else { return }  
 }
