a3aae45576351f9d55af56565c63152b46aeb727
Ev Bogue <ev@evbogue.com>
2018-04-25 15:43:27 -0500

    clean up

--- render.js
+++ render.js
@@ -1,13 +1,10 @@
 var h = require('hyperscript')
-
 var human = require('human-time')
 var ref = require('ssb-ref')
 var avatar = require('./avatar')
 
 var markdown = require('ssb-markdown')
 var config = require('./config')()
-
-var render = require('./render')
 
 function rawJSON (obj) {
   return JSON.stringify(obj, null, 2)
@@ -20,79 +17,59 @@ function rawJSON (obj) {
       })
 }
 
+function header (msg) {
+  return h('div.header', 
+    h('span.avatar',
+      h('a', {href: '#' + msg.value.author},
+        h('span.avatar--small', avatar.image(msg.value.author)),
+        avatar.name(msg.value.author)
+      )
+    ),
+    h('span.timestamp', h('a', {href: '#' + msg.key}, human(new Date(msg.value.timestamp)))),
+  )
+}
+
 module.exports = function (msg) {
+  var message = h('div.message')
+  message.appendChild(header(msg))
+
   if (msg.value.content.type == 'post') {
-    return h('div.message',
-      h('span.avatar', 
-        h('a', {href: '#' + msg.value.author},
-          h('span.avatar--small', avatar.image(msg.value.author)), 
-          avatar.name(msg.value.author)
-        )
-      ),
-      h('span.timestamp', h('a', {href: '#' + msg.key}, human(new Date(msg.value.timestamp)))),
-      h('div.message__body', 
+    message.appendChild(h('div.message__body', 
         {innerHTML: markdown.block(msg.value.content.text, {toUrl: function (url, image) {
-          if(url[0] == '@') return '#' + url
-          if(url[0] == '%') return '#' + url
+          if(url[0] == '%' || url[0] == '@') return '#' + url
           if(!image) return url
           if(url[0] !== '&') return url
-          return config.blobsurl + url
+          return config.blobsUrl + url
         }})}
       )
     )
+    return message
   } else if (msg.value.content.type == 'vote') {
     var embed = msg.value.content.vote.link
 
     var embedded = h('div.embedded')
-
     sbot.get(embed, function (err, msg) {
       if (err) {console.log('could not find message locally, try ooo?') }
-      console.log(msg)
       msg.value = msg
       msg.key = embed
       if (msg.value.content.text) {
-        embedded.appendChild(
-          h('div.message',
-            h('span.avatar',
-              h('a', {href: '#' + msg.value.author},
-                h('span.avatar--small', avatar.image(msg.value.author)),
-                avatar.name(msg.value.author)
-              )
-            ),
-            h('span.timestamp', h('a', {href: '#' + msg.key}, human(new Date(msg.value.timestamp)))),
-            h('div.message__body',
-              {innerHTML: markdown.block(msg.value.content.text, {toUrl: function (url, image) {
-                if(url[0] == '@') return '#' + url
-                if(url[0] == '%') return '#' + url
-                if(!image) return url
-                if(url[0] !== '&') return url
-                return config.blobsUrl + url
-              }})}
-            )
-          )
-        )
+        message.appendChild(h('img.emoji', {src: config.emojiUrl + 'star.png'}))
+        message.appendChild(embedded)
+        embedded.appendChild(header(msg))
+        embedded.appendChild(h('div.message__body',
+          {innerHTML: markdown.block(msg.value.content.text, {toUrl: function (url, image) {
+            if(url[0] == '@') return '#' + url
+            if(url[0] == '%') return '#' + url
+            if(!image) return url
+            if(url[0] !== '&') return url
+            return config.blobsUrl + url
+          }})}
+        ))
       }
     })
-    return h('div.message',
-      h('span.avatar',
-        h('a', {href: '#' + msg.value.author},
-          h('span.avatar--small', avatar.image(msg.value.author)),
-          avatar.name(msg.value.author)
-        ), h('img.emoji', {src: config.emojiUrl + 'star.png'})
-      ),
-      h('span.timestamp', h('a', {href: '#' + msg.key}, human(new Date(msg.value.timestamp)))),
-      h('div.message__body', embedded)
-    )
+    return message
   } else { 
-    return h('div.message',
-      h('span.avatar',
-        h('a', {href: '#' + msg.value.author},
-          h('span.avatar--small', avatar.image(msg.value.author)),
-          avatar.name(msg.value.author)
-        )
-      ),
-      h('span.timestamp', h('a', {href: '#' + msg.key}, human(new Date(msg.value.timestamp)))),
-      h('pre', rawJSON(msg.value.content))  
-    )
+    message.appendChild(h('pre', rawJSON(msg.value.content)))
+    return message
   }
 }
