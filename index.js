d783575b5b38608603e2a301844fb73d9d370541
Ev Bogue <ev@evbogue.com>
2018-05-16 13:44:41 -0500

    re-modularize routes into views.js

--- index.js
+++ index.js
@@ -1,111 +1,20 @@
-var pull = require('pull-stream')
-var sbot = require('./scuttlebot')
-var hyperscroll = require('hyperscroll')
-var More = require('pull-more')
-var stream = require('hyperloadmore/stream')
 var h = require('hyperscript')
-var render = require('./render')
-
-var ref = require('ssb-ref')
+var route = require('./views')
 
 document.head.appendChild(h('style', require('./style.css.json')))
-
-function hash () {
-  return window.location.hash.substring(1)
-}
 
 var content = h('div.content')
 
 var screen = h('div#screen', {style: {position: 'absolute', top: '0px', bottom: '0px', left: '0px', right: '0px'}})
 
-function route () {
-  var src = hash()
-
-  if (ref.isFeed(src)) {
-    var content = h('div.content')
-    var screen = document.getElementById('screen')
-    screen.appendChild(hyperscroll(content))
-    function createStream (opts) {
-      return pull(
-        More(sbot.userStream, opts, ['value', 'sequence']),
-        pull.map(function (msg) {
-          return h('div', render(msg))
-        })
-      )
-    }
-  
-    pull(
-      createStream({old: false, limit: 10, id: src}),
-      stream.top(content)
-    )
-  
-    pull(
-      createStream({reverse: true, live: false, limit: 10, id: src}),
-      stream.bottom(content)
-    )
-
-
-  } else if (ref.isMsg(src)) { 
-    var content = h('div.content')
-    var screen = document.getElementById('screen')
-    screen.appendChild(hyperscroll(content))
-    sbot.get(src, function (err, data) {
-      if (err) {console.log('could not find message') }
-      data.value = data
-      console.log(data)
-      var root = src
-      if (data.value.content.root)
-        root = data.value.content.root
-      sbot.get(root, function (err, data) {
-        if (err) { console.log('could not find root')}
-        data.value = data
-        data.key = root
-        content.appendChild(h('div', render(data)))
-        pull(
-          sbot.links({rel: 'root', dest: root, values: true, keys: true, live: true}),
-          pull.drain(function (msg) {
-            console.log(msg)
-            if (msg.value)
-              content.appendChild(h('div', render(msg)))
-          })
-        )
-      })
-    })
-  } else {
-    var content = h('div.content')
-    var screen = document.getElementById('screen')
-    screen.appendChild(hyperscroll(content))
-    function createStream (opts) {
-      return pull(
-        More(sbot.createLogStream, opts),
-        pull.map(function (msg) {
-          return h('div', render(msg))
-        })
-      )
-    }
-
-    pull(
-      createStream({old: false, limit: 10}),
-      stream.top(content)
-    )
-
-    pull(
-      createStream({reverse: true, live: false, limit: 10}),
-      stream.bottom(content)
-    )
-
-  }
-}
-
 document.body.appendChild(screen)
 route()
 
 window.onhashchange = function () {
-  //var content = h('div.content')
   var screen = document.getElementById('screen')
+
   var newscreen = h('div#screen', {style: {position: 'absolute', top: '0px', bottom: '0px', left: '0px', right: '0px'}})
 
-  //if(_screen != null)
   console.log(screen)
   screen.parentNode.replaceChild(newscreen, screen)
   route()
--- views.js
+++ views.js
@@ -5,140 +5,91 @@ var More = require('pull-more')
 var stream = require('hyperloadmore/stream')
 var h = require('hyperscript')
 var render = require('./render')
+var ref = require('ssb-ref')
 
 var compose = require('./compose')
 
-var content = h('div.content')
-function screen () {
-  document.body.appendChild(h('div.screen',
-    {style: {position: 'absolute', top: '0px', bottom: '0px', left: '0px', right: '0px'}},
-    hyperscroll(content)
-  ))
+function hash () {
+  return window.location.hash.substring(1)
 }
 
-module.exports.logstream = function () {
-  var opts = {
-    "type": 'post', 
-    "placeholder": 'Write a mutable message'
-  }
+module.exports = function () {
+  var src = hash()
 
-  var content = h('div.content',
-    h('div.message', compose(opts))
-  )
-
-  function mainscreen () {
-    document.body.appendChild(h('div.screen',
-      {style: {position: 'absolute', top: '0px', bottom: '0px', left: '0px', right: '0px'}},
-      hyperscroll(content)
-    ))
-  }
-
-  mainscreen()
-
-  function createStream (opts) {
-    return pull(
-      More(sbot.createLogStream, opts),
-      pull.map(function (msg) {
-        return h('div', render(msg))
-      })
-    )
-  }
-
-  pull(
-    createStream({old: false, limit: 10}),
-    stream.top(content)
-  )
-
-  pull(
-    createStream({reverse: true, live: false, limit: 10}),
-    stream.bottom(content)
-  )
-}
-
-var rawJSON = require('patchapp-raw/json')
-
-module.exports.rawstream = function () {
-  screen()
-
-  function createStream (opts) {
-    return pull(
-      More(sbot.createLogStream, opts),
-      pull.filter(function (data) {
-        return 'string' === typeof data.value.content.text
-      }),
-      pull.map(function (msg) {
-        return h('pre.raw__json', {id: msg.key}, rawJSON(msg))
-      })
-    )
-  }
-
-  pull(
-    createStream({old: false, limit: 10}),
-    stream.top(content)
-  )
-
-  pull(
-    createStream({reverse: true, live: false, limit: 10}),
-    stream.bottom(content)
-  )
-}
-
-module.exports.userstream = function (src) {
-  screen()
-
-  function createStream (opts) {
-    return pull(
-      More(sbot.userStream, opts, ['value', 'sequence']),
-      pull.map(function (msg) {
-        return h('div', render(msg))
-      })
-    )
-  }
-
-  pull(
-    createStream({old: false, limit: 10, id: src}),
-    stream.top(content)
-  )
-
-  pull(
-    createStream({reverse: true, live: false, limit: 10, id: src}),
-    stream.bottom(content)
-  )
-
-}
-
-module.exports.get = function (src) {
-  screen()
-
-  sbot.get(src, function (err, data) {
-    if (err) {console.log('could not find message') } 
-    data.value = data
-    console.log(data)
-    var root = src
-    if (data.value.content.root) 
-      root = data.value.content.root
-    sbot.get(root, function (err, data) {
-      if (err) { console.log('could not find root')}
-      data.value = data
-      data.key = root
-      content.appendChild(h('div', render(data)))
-      pull(
-        sbot.links({rel: 'root', dest: root, values: true, keys: true, live: true}),
-        pull.drain(function (msg) {
-          console.log(msg)
-          if (msg.value)
-            content.appendChild(h('div', render(msg)))
+  if (ref.isFeed(src)) {
+    var content = h('div.content')
+    var screen = document.getElementById('screen')
+    screen.appendChild(hyperscroll(content))
+    function createStream (opts) {
+      return pull(
+        More(sbot.userStream, opts, ['value', 'sequence']),
+        pull.map(function (msg) {
+          return h('div', render(msg))
         })
       )
-    })
-  })
-} 
+    }
 
-module.exports.compose = function () {
-  screen()
-  var opts = {
-    "root": null,
-    "type": "post"
+    pull(
+      createStream({old: false, limit: 10, id: src}),
+      stream.top(content)
+    )
+
+    pull(
+      createStream({reverse: true, live: false, limit: 10, id: src}),
+      stream.bottom(content)
+    )
+
+
+  } else if (ref.isMsg(src)) {
+    var content = h('div.content')
+    var screen = document.getElementById('screen')
+    screen.appendChild(hyperscroll(content))
+    sbot.get(src, function (err, data) {
+      if (err) {console.log('could not find message') }
+      data.value = data
+      console.log(data)
+      var root = src
+      if (data.value.content.root)
+        root = data.value.content.root
+      sbot.get(root, function (err, data) {
+        if (err) { console.log('could not find root')}
+        data.value = data
+        data.key = root
+        content.appendChild(h('div', render(data)))
+        pull(
+          sbot.links({rel: 'root', dest: root, values: true, keys: true, live: true}),
+          pull.drain(function (msg) {
+            console.log(msg)
+            if (msg.value)
+              content.appendChild(h('div', render(msg)))
+          })
+        )
+      })
+    })
+  } else {
+    var content = h('div.content')
+    var screen = document.getElementById('screen')
+    screen.appendChild(hyperscroll(content))
+    function createStream (opts) {
+      return pull(
+        More(sbot.createLogStream, opts),
+        pull.map(function (msg) {
+          return h('div', render(msg))
+        })
+      )
+    }
+
+    pull(
+      createStream({old: false, limit: 10}),
+      stream.top(content)
+    )
+
+    pull(
+      createStream({reverse: true, live: false, limit: 10}),
+      stream.bottom(content)
+    )
+
   }
-  content.appendChild(h('div.message', compose(opts)))
 }
+
+
