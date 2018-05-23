3684a134e04665d9b2a1f3088d936ac5af922e38
Ev Bogue <ev@evbogue.com>
2018-05-22 20:43:53 -0500

    add navbar and navbar styles

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 var h = require('hyperscript')
 var route = require('./views')
+var avatar = require('./avatar')
+
+var id = require('./keys').id
 
 document.head.appendChild(h('style', require('./style.css.json')))
 
-var content = h('div.content')
+var screen = h('div#screen', {style: {position: 'absolute', top: '35px', bottom: '0px', left: '0px', right: '0px'}})
 
-var screen = h('div#screen', {style: {position: 'absolute', top: '0px', bottom: '0px', left: '0px', right: '0px'}})
+var nav = h('div.navbar',
+  h('div.internal',
+    h('li', h('a', {href: '#' + id}, h('span.avatar--small', avatar.image(id)))),
+    h('li', h('a', {href: '#' + id}, avatar.name(id))),
+    h('li', h('a', {href: '#'}, 'Public'))
+  )
+)
 
+document.body.appendChild(nav)
 document.body.appendChild(screen)
 route()
 
 window.onhashchange = function () {
-  var screen = document.getElementById('screen')
-
-  var newscreen = h('div#screen', {style: {position: 'absolute', top: '0px', bottom: '0px', left: '0px', right: '0px'}})
-
-  console.log(screen)
-  screen.parentNode.replaceChild(newscreen, screen)
+  var oldscreen = document.getElementById('screen')
+  var newscreen = h('div#screen', {style: {position: 'absolute', top: '35px', bottom: '0px', left: '0px', right: '0px'}})
+  oldscreen.parentNode.replaceChild(newscreen, oldscreen)
   route()
 }
 
