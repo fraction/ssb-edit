86661677a7623472280bc05e563bb733054a0f01
Ev Bogue <ev@evbogue.com>
2018-05-25 20:13:21 -0500

    edits should cancel correctly

--- compose.js
+++ compose.js
@@ -41,6 +41,8 @@ module.exports = function (opts, buttons) {
   var files = []
   var filesById = {}
 
+  console.log(opts)
+
   var composer = h('div.composer')
   var container = h('div.container')
 
@@ -49,28 +51,24 @@ module.exports = function (opts, buttons) {
   else
     var textarea = h('textarea.compose', {placeholder: opts.placeholder || 'Write a message'})
 
-  if (window.location.hash.substring(1) != 'compose')
-    var cancelBtn = h('button.btn', 'Cancel', {
-      onclick: function () {
-        var cancel
-        if (opts.type == 'updated') {
-          cancel = document.getElementById(opts.updated.substring(0,10))
-          var oldMessage = h('div.message__body', tools.markdown(opts.messageText))
-          cancel.parentNode.replaceChild(oldMessage, cancel)
-          oldMessage.parentNode.appendChild(buttons)
-        } else {
-          if (document.getElementById(opts.branch.substring(0,10)))
-            cancel = document.getElementById(opts.branch.substring(0,10))
-          else
-            cancel = document.getElementById('composer')
-          cancel.parentNode.removeChild(cancel)
-        }
+  var cancelBtn = h('button.btn', 'Cancel', {
+    onclick: function () {
+      var cancel
+      if (opts.updated) {
+        cancel = document.getElementById(opts.updated.substring(0,10))
+        var oldMessage = h('div.message__body', tools.markdown(opts.messageText))
+        cancel.parentNode.replaceChild(oldMessage, cancel)
+        oldMessage.parentNode.appendChild(buttons)
+      } else if (opts.branch) {
+        cancel = document.getElementById(opts.branch.substring(0,10))
+        cancel.parentNode.removeChild(cancel)
+      } else {
+        cancel = document.getElementById('composer')
+        cancel.parentNode.removeChild(cancel)
       }
+    }
 
-    })
- 
-  else 
-    var cancel = h('span', '')
+  })
 
   var initialButtons = h('span', 
     h('button.btn', 'Preview', {
