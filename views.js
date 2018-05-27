8cbf61e71d58d05291bf568722a7edeb856f671b
Ev Bogue <ev@evbogue.com>
2018-05-27 18:55:01 -0500

    move views into their own functions

--- views.js
+++ views.js
@@ -13,15 +13,43 @@ var fs = require('fs')
 
 var compose = require('./compose')
 
-function hash () {
-  return window.location.hash.substring(1)
+var about = function () {
+  var screen = document.getElementById('screen')
+
+  var about = require('./about')
+
+  var content = h('div.content', about)
+
+  screen.appendChild(hyperscroll(content))
 }
 
-module.exports = function () {
-  var src = hash()
+var logStream = function () {
+  var content = h('div.content')
+  var screen = document.getElementById('screen')
+  screen.appendChild(hyperscroll(content))
 
-  if (ref.isFeed(src)) {
-    var content = h('div.content')
+  function createStream (opts) {
+    return pull(
+      More(sbot.createLogStream, opts),
+      pull.map(function (msg) {
+        return render(msg)
+      })
+    )
+  }
+
+  pull(
+    createStream({old: false, limit: 10}),
+    stream.top(content)
+  )
+
+  pull(
+    createStream({reverse: true, live: false, limit: 10}),
+    stream.bottom(content)
+  )
+}
+
+var userStream = function (src) {
+  var content = h('div.content')
     var screen = document.getElementById('screen')
     screen.appendChild(hyperscroll(content))
     function createStream (opts) {
@@ -43,105 +71,89 @@ module.exports = function () {
       stream.bottom(content)
     )
 
+}
 
-  } else if (ref.isMsg(src)) {
-    var content = h('div.content')
-    var screen = document.getElementById('screen')
-    screen.appendChild(hyperscroll(content))
-    sbot.get(src, function (err, data) {
-      if (err) {console.log('could not find message') }
+var msgThread = function(src) {
+  var content = h('div.content')
+  var screen = document.getElementById('screen')
+  screen.appendChild(hyperscroll(content))
+  sbot.get(src, function (err, data) {
+    if (err) {console.log('could not find message') }
+    data.value = data
+    console.log(data)
+    var root = src
+    if (data.value.content.root)
+      root = data.value.content.root
+    sbot.get(root, function (err, data) {
+      if (err) { console.log('could not find root')}
       data.value = data
-      console.log(data)
-      var root = src
-      if (data.value.content.root)
-        root = data.value.content.root
-      sbot.get(root, function (err, data) {
-        if (err) { console.log('could not find root')}
-        data.value = data
-        data.key = root
-        content.appendChild(render(data))
-        pull(
-          sbot.links({rel: 'root', dest: root, values: true, keys: true, live: true}),
-          pull.drain(function (msg) {
-            console.log(msg)
-            if (msg.value)
-              content.appendChild(render(msg))
-          })
-        )
-      })
-    })
-  } 
-
-  else if (src == 'about') {
-
-    var screen = document.getElementById('screen')
-
-    var about = require('./about')
-
-    var content = h('div.content', about)
-
-    screen.appendChild(hyperscroll(content))
-  } 
-  
-  else if (src == 'key') {
-    var screen = document.getElementById('screen')
-
-    var importKey = h('textarea.import', {placeholder: 'Import a new public/private key', name: 'textarea', style: 'width: 97%; height: 100px;'})
-
-    var content = h('div.content',
-      h('div.message#key',
-        h('h1', 'Your Key'),
-        h('p', {innerHTML: 'Your public/private key is: <pre><code>' + localStorage[config.caps.shs + '/secret'] + '</code></pre>'},
-        h('button.btn', {onclick: function (e){
-            localStorage[config.caps.shs +'/secret'] = ''
-            alert('Your public/private key has been deleted')
-            e.preventDefault()
-            location.hash = ""
-            location.reload()
-          }}, 'Delete Key')
-        ),
-        h('hr'),
-        h('form',
-          importKey,
-          h('button.btn', {onclick: function (e){
-            if(importKey.value) {
-              localStorage[config.caps.shs + '/secret'] = importKey.value.replace(/\s+/g, ' ')
-              e.preventDefault()
-              alert('Your public/private key has been updated')
-            }
-            location.hash = ""
-            location.reload()
-          }}, 'Import key'),
-        )
-      )
-    )
-  
-    screen.appendChild(hyperscroll(content))
-
-  } else {
-    var content = h('div.content')
-    var screen = document.getElementById('screen')
-    screen.appendChild(hyperscroll(content))
-    function createStream (opts) {
-      return pull(
-        More(sbot.createLogStream, opts),
-        pull.map(function (msg) {
-          return render(msg)
+      data.key = root
+      content.appendChild(render(data))
+      pull(
+        sbot.links({rel: 'root', dest: root, values: true, keys: true, live: true}),
+        pull.drain(function (msg) {
+          console.log(msg)
+          if (msg.value)
+            content.appendChild(render(msg))
         })
       )
-    }
+    })
+  })
+}
 
-    pull(
-      createStream({old: false, limit: 10}),
-      stream.top(content)
+var keyPage = function () {
+  var screen = document.getElementById('screen')
+
+  var importKey = h('textarea.import', {placeholder: 'Import a new public/private key', name: 'textarea', style: 'width: 97%; height: 100px;'})
+
+  var content = h('div.content',
+    h('div.message#key',
+      h('h1', 'Your Key'),
+      h('p', {innerHTML: 'Your public/private key is: <pre><code>' + localStorage[config.caps.shs + '/secret'] + '</code></pre>'},
+      h('button.btn', {onclick: function (e){
+          localStorage[config.caps.shs +'/secret'] = ''
+          alert('Your public/private key has been deleted')
+          e.preventDefault()
+          location.hash = ""
+          location.reload()
+        }}, 'Delete Key')
+      ),
+      h('hr'),
+      h('form',
+        importKey,
+        h('button.btn', {onclick: function (e){
+          if(importKey.value) {
+            localStorage[config.caps.shs + '/secret'] = importKey.value.replace(/\s+/g, ' ')
+            e.preventDefault()
+            alert('Your public/private key has been updated')
+          }
+          location.hash = ""
+          location.reload()
+        }}, 'Import key'),
+      )
     )
+  )
 
-    pull(
-      createStream({reverse: true, live: false, limit: 10}),
-      stream.bottom(content)
-    )
-
-  }
+  screen.appendChild(hyperscroll(content))
 }
 
 
+function hash () {
+  return window.location.hash.substring(1)
+}
+
+module.exports = function () {
+  var src = hash()
+
+  if (ref.isFeed(src)) {
+    userStream(src)
+  } else if (ref.isMsg(src)) {
+    msgThread(src)
+  } else if (src == 'about') {
+    about()
+  } else if (src == 'key') {
+    keyPage()
+  } else {
+    logStream()
+  }
+}
