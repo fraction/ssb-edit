4c19550a991fe099117a720ba1ac9e2b7db1fc6e
Ev Bogue <ev@evbogue.com>
2018-05-27 20:28:08 -0500

    live queries for edits

--- render.js
+++ render.js
@@ -35,18 +35,23 @@ module.exports = function (msg) {
 
 
     pull(
-      sbot.query({query: [{$filter: {value: {content: {type: 'edit', original: msg.key}}}}]}),
+      sbot.query({query: [{$filter: {value: {content: {type: 'edit', original: msg.key}}}}], limit: 100, live: true}),
       pull.drain(function (update) {
-        var newMessage = h('div', tools.markdown(update.value.content.text))
-        var latest = h('div.message__body', 
-          tools.timestamp(update, {edited: true}),
-          newMessage
-        )
-        message.replaceChild(latest, message.childNodes[message.childNodes.length - 2])
-        fallback.messageText = update.value.content.text
-        opts.updated = update.key
-        opts.original = msg.key
-      })
+        console.log(update)
+        if (update.sync) { 
+          console.log('Waiting for new edits.')
+        } else {
+          var newMessage = h('div', tools.markdown(update.value.content.text))
+          var latest = h('div.message__body', 
+            tools.timestamp(update, {edited: true}),
+            newMessage
+          )
+          message.replaceChild(latest, message.childNodes[message.childNodes.length - 2])
+          fallback.messageText = update.value.content.text
+          opts.updated = update.key
+          opts.original = msg.key
+        } 
+      })    
     )
 
     var buttons = h('div.buttons')
