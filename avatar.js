77f26b50550020fe2795e58ee7b816df90cdca45
Ev Bogue <ev@evbogue.com>
2018-06-25 18:39:52 -0500

    make sure that key is a valid feedId before checking avatar

--- avatar.js
+++ avatar.js
@@ -11,47 +11,14 @@ var config = require('./config')()
 
 var id = require('./keys').id
 
+var ref = require('ssb-ref')
+
 module.exports.name = function (key) {
   
   var avatarname = h('span', key.substring(0, 10))
-
-  avatar(sbot, id, key, function (err, data) {
-    if (err) throw err
-    if (data.name) {
-      if (data.name[0] != '@') {
-        var name = '@' + data.name
-      } else {
-        var name = data.name
-      }
-      localStorage[key + 'name'] = name
-      avatarname.textContent = name
-    }
-  }) 
-
-  return avatarname
-}
-
-module.exports.image = function (key) {
-  var img = visualize(new Buffer(key.substring(1), 'base64'), 256)
-
-  avatar(sbot, id, key, function (err, data) {
-    if (err) throw err
-    if (data.image) {
-      localStorage[key + 'image'] = data.image
-      img.src = config.blobsUrl + data.image 
-    }
-  })
-
-  return img
-}
-
-module.exports.cachedName = function (key) {
-  var avatarname = h('span', key.substring(0, 10))
-
-  if (localStorage[key + 'name']) {
-    avatarname.textContent = localStorage[key + 'name']
-  } else {
+  if (ref.isFeedId(key)) {
     avatar(sbot, id, key, function (err, data) {
+      if (err) throw err
       if (data.name) {
         if (data.name[0] != '@') {
           var name = '@' + data.name
@@ -61,7 +28,45 @@ module.exports.cachedName = function (key) {
         localStorage[key + 'name'] = name
         avatarname.textContent = name
       }
+    }) 
+  }
+  return avatarname
+}
+
+module.exports.image = function (key) {
+  var img = visualize(new Buffer(key.substring(1), 'base64'), 256)
+
+  if (ref.isFeedId(key)) {
+    avatar(sbot, id, key, function (err, data) {
+      if (err) throw err
+      if (data.image) {
+        localStorage[key + 'image'] = data.image
+        img.src = config.blobsUrl + data.image 
+      }
     })
+  }
+  return img
+}
+
+module.exports.cachedName = function (key) {
+  var avatarname = h('span', key.substring(0, 10))
+
+  if (localStorage[key + 'name']) {
+    avatarname.textContent = localStorage[key + 'name']
+  } else {
+    if (ref.isFeedId(key)) {
+      avatar(sbot, id, key, function (err, data) {
+        if (data.name) {
+          if (data.name[0] != '@') {
+            var name = '@' + data.name
+          } else {
+            var name = data.name
+          }
+          localStorage[key + 'name'] = name
+          avatarname.textContent = name
+        }
+      })
+    }
   } 
 
   return avatarname
@@ -73,12 +78,14 @@ module.exports.cachedImage = function (key) {
   if (localStorage[key + 'image']) {
     img.src = config.blobsUrl + localStorage[key + 'image']
   } else {
-    avatar(sbot, id, key, function (err, data) {
-      if (data.image) {
-        localStorage[key + 'image'] = data.image
-        img.src = config.blobsUrl + data.image
-      }
-    })
+    if (ref.isFeedId(key)) {
+      avatar(sbot, id, key, function (err, data) {
+        if (data.image) {
+          localStorage[key + 'image'] = data.image
+          img.src = config.blobsUrl + data.image
+        }
+      })
+    }
   }
 
   return img
