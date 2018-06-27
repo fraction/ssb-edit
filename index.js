b708d9451bf87212991f70650ab8815c255380bd
Ev Bogue <ev@evbogue.com>
2018-06-27 08:56:17 -0500

    add chat to the top nav

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ var nav = h('div.navbar',
     h('li', h('a', {href: '#private' }, 'Private')),
     h('li', h('a', {href: '#mentions' }, 'Mentions')),
     h('li', h('a', {href: '#key' }, 'Key')),
+    h('li', h('a', {href: '#backchannel' }, 'Chat')),
     h('li.right', h('a', {href: '#about'}, '?'))
   )
 )
