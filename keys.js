772af228084d2b97e247e64daae8b7e08007186c
Christian Bundy <christianbundy@fraction.io>
2018-12-21 18:47:04 -0800

    Remove unrelated files

--- keys.js
+++
@@ -1,6 +0,0 @@
-
-var config = require('./config')()
-var ssbKeys      = require('ssb-keys')
-var path         = require('path')
-
-module.exports = ssbKeys.loadOrCreateSync(path.join(config.caps.shs + '/secret'))
